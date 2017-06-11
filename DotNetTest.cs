8e4e00bf8c367f138143697e045dc7c35dc0d4fd
Bart Wijnants <bartwijnants@outlook.com>
2017-06-11 08:00:15 +0200

    initial commit

---
+++ DotNetTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace dotnetcore_node_travis
+{
+    public class DotNetTest
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(false, true);
+        }
+    }
+}
